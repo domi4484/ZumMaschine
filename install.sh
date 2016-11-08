255f88070112fe35f334bbdb5973996fba9b3255
Damiano Lombardi\n <lombardi.damiano@gmail.com>
2016-11-08 23:37:45 +0100

    install

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Install icon
+echo "Install icon"
 mkdir -p ~/.local/share/ZumMaschine
 cp ZumMaschine.png ~/.local/share/ZumMaschine/
 
-# Install Nemo extension
+echo "Install Nemo extension"
 NEMO_EXTENSIONS=~/.local/share/nemo-python/extensions
 mkdir -p $NEMO_EXTENSIONS
 cp ZumMaschineNemo.py $NEMO_EXTENSIONS
 
-# Install Nautilus extension
+echo "Install Nautilus extension"
 NAUTILUS_EXTENSIONS=~/.local/share/nautilus-python/extensions
 mkdir -p $NAUTILUS_EXTENSIONS
 cp ZumMaschineNautilus.py $NAUTILUS_EXTENSIONS
